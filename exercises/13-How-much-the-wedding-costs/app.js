d83bf89ed7f191a5d81858f3ab9b2539db54642c
Brandchal <Bchavarrial@hotmail.com>
2021-02-14 23:29:32 +0000

    update

--- exercises/13-How-much-the-wedding-costs/app.js
+++ exercises/13-How-much-the-wedding-costs/app.js
@@ -1,10 +1,22 @@
-let guests = prompt('How many people are coming to your wedding?');
+let guests = parseInt(prompt('How many people are coming to your wedding?'));
 
 function getPrice(guests){
     let cost = 0;
     // Your code here
+    if (guests <=50) {
+        cost = 4000;
+    } else if(guests > 50 && guests <= 100) {
+        cost = 10000;
+    
+    } else if(guests > 100 && guests <= 200) {
+        cost = 15000;
+
+    } else if(guests > 200) {
+        cost = 20000;
+    }
+
     return cost;
 }
 
-let price = getPrice(input);
+let price = getPrice(guests);
 console.log('Your wedding will cost '+price+' dollars');

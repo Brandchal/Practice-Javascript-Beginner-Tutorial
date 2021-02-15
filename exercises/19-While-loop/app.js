00f1a7da64a3cb4a76a0fbdb3b3d6e770b93d06f
Brandchal <Bchavarrial@hotmail.com>
2021-02-15 04:57:29 +0000

    update

--- exercises/19-While-loop/app.js
+++ exercises/19-While-loop/app.js
@@ -1,15 +1,14 @@
 
 //fix this function:
-function startCounting()
-{
-	var counter = 100;
-	while(counter <= 100)
-	{
-		counter --;
-		console.log(counter);
-	}
-	
-	return counter;
+function startCounting() {
+    var counter = 100;
+
+    while (counter <= 100 && counter >= 0) {
+        console.log(counter);
+        counter = counter - 1;
+    }
+
+    return counter;
 }
 
 startCounting();

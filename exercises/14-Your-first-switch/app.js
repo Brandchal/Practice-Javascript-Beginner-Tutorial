edef30d8b6d2b66c4dc23ad6685ab8f8494be902
Brandchal <Bchavarrial@hotmail.com>
2021-02-15 01:39:40 +0000

    update

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,17 +1,27 @@
-function getColor(selection)
-{
-	switch(selection){
-		// Add more options here
-	    default:
-	    	return false;  //returns false because the user picked an unavailable color
-	    break;               
-	}
-}
+function getColor(selection) {
+    switch (selection) {
+        // Add more options here
+     case "ROJO":
+            return true;
+            break;
 
-var colorname = prompt('What color do you want?');
+        case "AZUL":
+            return true;
+            break;
+
+        case "VERDE":
+            return true;
+            break;
+
+        default:
+            return false;  //returns false because the user picked an unavailable color
+            break;
+    }}
+var colorname = prompt('What color do you want?').toUpperCase();
 var isAvailable = getColor(colorname);
 
-if(isAvailable)
-	console.log('Good news! That color is available');
-else 
-	console.log('We are sorry, that color is not available');
+if (isAvailable)
+    console.log('Good news! That color is available');
+else
+    console.log('We are sorry, that color is not available');
+

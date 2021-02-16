5c711e3e65c8730c7673b2d0c6b919d7eb50b993
Brandchal <Bchavarrial@hotmail.com>
2021-02-16 17:47:48 +0000

    update

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,5 +1,27 @@
-function fizzBuzz() {  
-	// Your code here
+function fizzBuzz() {
+    // Your code here
+    function multiple(valor, multiple) {
+        resto = valor % multiple;
+        if (resto == 0)
+            return true;
+        else
+            return false;
+    }
+    for (let index = 1; index <= 100; index++) {
+
+        if (multiple(index, 3)) {
+            console.log("Fizz");
+        }
+        else if (multiple(index, 5)) {
+            console.log("Buzz");
+        } else {
+            console.log(index);
+        }
+
+    }
+
+
+
 }
 
 fizzBuzz();
